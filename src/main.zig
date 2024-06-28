59edefeb8bea5f6f46bf3e76d0366b1a32119f34
Craig <craig.collier@ro.co>
2024-06-28 00:20:54 -0700

    wip tesitn

--- src/main.zig
+++ src/main.zig
@@ -16,13 +16,13 @@ pub fn main() !void {
 pub fn cpumain() !void {
     // const file = try std.fs.cwd().openFile("tetris.gb", .{});
     // pass
-    const file = try std.fs.cwd().openFile("./01-special.gb", .{});
+    // const file = try std.fs.cwd().openFile("./01-special.gb", .{});
     // fail
     // const file = try std.fs.cwd().openFile("./02-interrupts.gb", .{});
     // pass
     // const file = try std.fs.cwd().openFile("./03-op sp,hl.gb", .{});
     // const file = try std.fs.cwd().openFile("./04-op r,imm.gb", .{});
-    // const file = try std.fs.cwd().openFile("./05-op rp.gb", .{});
+    const file = try std.fs.cwd().openFile("./05-op rp.gb", .{});
     // const file = try std.fs.cwd().openFile("./06-ld r,r.gb", .{});
     // const file = try std.fs.cwd().openFile("./07-jr,jp,call,ret,rst.gb", .{});
     // const file = try std.fs.cwd().openFile("./08-misc instrs.gb", .{});
