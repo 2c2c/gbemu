4d273331af3dedd3e89dc758021f4a6abe1d7133
Craig <craig.2c2c+git@gmail.com>
2025-02-20 11:53:20 -0800

    0.14.0 works

--- build.zig
+++ build.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
-const sdl = @import("SDL.zig/build.zig");
+const sdl = @import("sdl");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const sdk = sdl.init(b, null);
+    const sdk = sdl.init(b, .{});
 
     // link build main exe
     const exe = b.addExecutable(.{
         .name = "GBEMU",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
     // sdl2
-    sdk.link(exe, .dynamic);
+    sdk.link(exe, .dynamic, .SDL2);
     // exe.root_module.addImport("sdl2", sdk.getWrapperModule());
     exe.root_module.addImport("sdl2", sdk.getNativeModule());
 
@@ -31,27 +31,26 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // partial buildfor zls
+    // // partial buildfor zls
     const exe_check = b.addExecutable(.{
         .name = "GBEMU",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    sdk.link(exe_check, .dynamic);
+    sdk.link(exe_check, .dynamic, .SDL2);
     exe_check.root_module.addImport("sdl2", sdk.getNativeModule());
     const check = b.step("check", "Check the app");
     check.dependOn(&exe_check.step);
 
-    // unit tests
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
     exe_unit_tests.root_module.addImport("sdl2", sdk.getNativeModule());
-    sdk.link(exe_unit_tests, .dynamic);
+    sdk.link(exe_unit_tests, .dynamic, .SDL2);
     b.installArtifact(exe_unit_tests);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
